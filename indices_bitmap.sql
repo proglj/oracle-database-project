4147060a00d601278a221386086f41a93524b2f3
Josiel Santos <josiel25atx@outlook.com>
2025-01-25 20:17:38 -0300

    Create indices_bitmap.sql

---
+++ indices_bitmap.sql
@@ -0,0 +1,5 @@
+CREATE BITMAP INDEX idx_bitmap_status ON Clientes(status);
+
+CREATE BITMAP INDEX idx_bitmap_tipo ON Clientes(tipo_cliente);
+
+CREATE BITMAP INDEX idx_bitmap_regiao ON Clientes(regiao);
